c9660106ab8018da50c2b2a0688a02f65035edd8
Sintiya <sintiyarisla2005@gmail.com>
2026-04-25 13:52:48 +0700

    Fix modelling for CI

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,8 +5,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-mlflow.set_experiment("Telco Churn Prediction Basic")
-
 DATA_PATH = 'dataset_preprocessing/data_clean.csv'
 
 print("Memulai proses training model...")
@@ -46,24 +44,25 @@ print("Data berhasil dibagi.")
 
 mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="telco-logistic-regression"):
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
 
-    y_pred = model.predict(X_test)
+accuracy = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 
-    accuracy = accuracy_score(y_test, y_pred)
-    precision = precision_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
-    f1 = f1_score(y_test, y_pred)
+mlflow.log_metrics({
+    "accuracy": accuracy,
+    "precision": precision,
+    "recall": recall,
+    "f1_score": f1
+})
 
-    mlflow.log_metrics({
-        "accuracy": accuracy,
-        "precision": precision,
-        "recall": recall,
-        "f1_score": f1
-    })
+mlflow.sklearn.log_model(model, "model")
 
-    print("\nTraining selesai dan tercatat di MLflow.")
-    print(f"Akurasi: {accuracy:.4f}")
+print("\nTraining selesai dan tercatat di MLflow.")
+print(f"Akurasi: {accuracy:.4f}")
+print(f"F1 Score: {f1:.4f}")
